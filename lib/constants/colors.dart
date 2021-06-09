4ce6e1907f8bbfd7c84925e43a9b5e73ccce871d
Narendra ML <narenml2000@gmail.com>
2021-06-09 15:09:09 +0530

    Added colors to the app

---
+++ lib/constants/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color kBackground = Color(0xFF091D3B);
+const Color kOrange = Color(0xFCF9755B);
+const Color kPurple = Color(0xFF9B3490);
+const Color kBlue = Color(0xFF8343D3);
