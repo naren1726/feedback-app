ce777cd6bd94eb845715996dd1cf9717722a39ae
Narendra ML <narenml2000@gmail.com>
2021-06-09 10:04:32 +0530

    Added few additions to UI

--- lib/pages/feedback.dart
+++ lib/pages/feedback.dart
@@ -1,14 +1,9 @@
+import 'package:feedback/pages/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 // ignore: must_be_immutable
 class MyApp extends StatelessWidget {
-  var onStarPressed;
-
-  MyApp({this.onHeartPressed, this.isStar = false});
-  final VoidCallback onHeartPressed;
-  final bool isStar;
-
-  get inputType => null;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,102 +21,99 @@ class MyApp extends StatelessWidget {
         padding: EdgeInsets.all(10),
         child: ListView(
           children: [
-            SizedBox(height: 40),
-            Align(
-              alignment: Alignment.topLeft,
-              child: Text(
-                "How is did we serve?",
-                style: proStyle(),
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                starIcon(),
-                starIcon(),
-                starIcon(),
-                starIcon(),
-                starIcon(),
-              ],
-            ),
-            SizedBox(height: 20),
-            Align(
-              alignment: Alignment.topLeft,
-              child: Text(
-                "How is our products?",
-                style: proStyle(),
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                starIcon(),
-                starIcon(),
-                starIcon(),
-                starIcon(),
-                starIcon(),
-              ],
-            ),
-            SizedBox(height: 40),
-            Align(
-              alignment: Alignment.center,
-              child: Text(
-                "Please your feedback here...",
-                style: proStyle(),
-              ),
-            ),
-            SizedBox(height: 10),
-            Container(
-                height: 250,
-                width: 300,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: Colors.white,
-                  border: Border.all(color: Color(0xFFABA8A8), width: 2),
+            Card(
+              elevation: 2.0,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 1),
+                child: Row(
+                  children: [
+                    Image(
+                      image: AssetImage("assets/images/shop.jpg"),
+                      height: 50,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(10),
+                          child: Text(
+                            'Vishal Super Mart,  RT-Nagar Branch',
+                            style:
+                                Theme.of(context).textTheme.bodyText1.copyWith(
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                          ),
+                        ),
+                        Padding(
+                          padding:
+                              const EdgeInsets.only(left: 10.0, bottom: 8.0),
+                          child: Text(
+                            'RT-Nagar, Church Street, Bengaluru - 560094',
+                            style: Theme.of(context).textTheme.caption.copyWith(
+                                color: Theme.of(context).disabledColor,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 11),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
-                child: TextFormField(
-                  cursorColor: Colors.black,
-                  keyboardType: inputType,
-                  decoration: new InputDecoration(
-                    border: InputBorder.none,
-                    focusedBorder: InputBorder.none,
-                    enabledBorder: InputBorder.none,
-                    errorBorder: InputBorder.none,
-                    disabledBorder: InputBorder.none,
-                    contentPadding: EdgeInsets.only(
-                        left: 15, bottom: 11, top: 11, right: 15),
-                  ),
-                )),
+              ),
+            ),
             SizedBox(height: 20),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                padding: EdgeInsets.symmetric(vertical: 20, horizontal: 70),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(5)),
-                textStyle: TextStyle(color: Color(0xFF8BF4D0)),
-                primary: Color(0xFF8BF4D0),
-              ),
-              child: Text(
-                'PUBLISH YOUR FEEDBACK',
-                style: TextStyle(
-                    color: Colors.blue[900],
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'WorkSansMedium'),
-              ),
-              onPressed: () {
-                showAlertDialog(context);
+            Text(
+              "Rate your Experience",
+              style: headingStyle(),
+            ),
+            SizedBox(height: 5),
+            Text(
+              "Please rate your recent visit to the store",
+              style: subHeadingStyle(),
+            ),
+            SizedBox(height: 20),
+            RatingBar.builder(
+              initialRating: 0,
+              itemCount: 5,
+              itemBuilder: (context, index) {
+                switch (index) {
+                  case 0:
+                    return Icon(
+                      Icons.sentiment_very_dissatisfied,
+                      color: Colors.red,
+                    );
+                  case 1:
+                    return Icon(
+                      Icons.sentiment_dissatisfied,
+                      color: Colors.redAccent,
+                    );
+                  case 2:
+                    return Icon(
+                      Icons.sentiment_neutral,
+                      color: Colors.amber,
+                    );
+                  case 3:
+                    return Icon(
+                      Icons.sentiment_satisfied,
+                      color: Colors.lightGreen,
+                    );
+                  case 4:
+                    return Icon(
+                      Icons.sentiment_very_satisfied,
+                      color: Colors.green,
+                    );
+                  default:
+                    return Icon(
+                      Icons.sentiment_very_dissatisfied,
+                      color: Colors.red,
+                    );
+                }
+              },
+              onRatingUpdate: (rating) {
+                print(rating);
               },
             ),
-            SizedBox(height: 20),
-            Align(
-              alignment: Alignment.center,
-              child: Text(
-                "Your review will be posted to Google Play store",
-                style: TextStyle(
-                  color: Color(0xFFABA8A8),
-                ),
-              ),
-            ),
+            RatingComments()
           ],
         ),
       ),
@@ -129,66 +121,21 @@ class MyApp extends StatelessWidget {
   }
 }
 
-IconButton starIcon() {
-  var onHeartPressed;
-  return IconButton(
-      icon: (Icons.star_border) != null
-          ? Icon(
-              Icons.star,
-              size: 40,
-              color: Color(0xFFF9D50E),
-            )
-          : Icon(Icons.star),
-      onPressed: onHeartPressed);
-}
-
-TextStyle proStyle() {
+TextStyle headingStyle() {
   return TextStyle(
     color: Colors.black,
-    fontWeight: FontWeight.bold,
+    fontWeight: FontWeight.w500,
+    fontSize: 20,
   );
 }
 
-showAlertDialog(BuildContext context) {
-  // set up the AlertDialog
-  AlertDialog alert = AlertDialog(
-    content: Container(
-      decoration: BoxDecoration(borderRadius: BorderRadius.circular(20)),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Image(image: AssetImage('assets/images/feedback.png')),
-          // SizedBox(height: 20),
-          Text(
-            "Submitted!",
-            style: TextStyle(
-              fontSize: 25,
-              color: Colors.black,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          // SizedBox(height: 20),
-          Text(
-            "Thank you for sharing your thoughts",
-            style: TextStyle(
-              color: Colors.black,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          // Image(
-          //   image: AssetImage('assets/images/tick.jpg'),
-          // ),
-        ],
-      ),
-    ),
+TextStyle subHeadingStyle() {
+  return TextStyle(
+    color: Colors.black45,
+    fontWeight: FontWeight.w600,
+    fontSize: 12,
   );
+}
 
   // show the dialog
-  showDialog(
-    context: context,
-    builder: (BuildContext context) {
-      return alert;
-    },
-  );
-}
+
---
+++ lib/pages/widgets.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+class RatingComments extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+            height: 250,
+            width: 300,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: Colors.white,
+              border: Border.all(color: Color(0xFFABA8A8), width: 2),
+            ),
+            child: TextFormField(
+              cursorColor: Colors.black,
+              keyboardType: TextInputType.text,
+              decoration: new InputDecoration(
+                border: InputBorder.none,
+                focusedBorder: InputBorder.none,
+                enabledBorder: InputBorder.none,
+                errorBorder: InputBorder.none,
+                disabledBorder: InputBorder.none,
+                contentPadding:
+                    EdgeInsets.only(left: 15, bottom: 11, top: 11, right: 15),
+              ),
+            )),
+        SizedBox(height: 20),
+        ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            padding: EdgeInsets.symmetric(vertical: 20, horizontal: 70),
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+            textStyle: TextStyle(color: Color(0xFF8BF4D0)),
+            primary: Color(0xFF8BF4D0),
+          ),
+          child: Text(
+            'PUBLISH YOUR FEEDBACK',
+            style: TextStyle(
+                color: Colors.blue[900],
+                fontWeight: FontWeight.bold,
+                fontFamily: 'WorkSansMedium'),
+          ),
+          onPressed: () {
+            showAlertDialog(context);
+          },
+        ),
+      ],
+    );
+  }
+
+  showAlertDialog(BuildContext context) {
+    // set up the AlertDialog
+    AlertDialog alert = AlertDialog(
+      content: Container(
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(20)),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image(image: AssetImage('assets/images/feedback.png')),
+            // SizedBox(height: 20),
+            Text(
+              "Submitted!",
+              style: TextStyle(
+                fontSize: 25,
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            // SizedBox(height: 20),
+            Text(
+              "Thank you for sharing your thoughts",
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            // Image(
+            //   image: AssetImage('assets/images/tick.jpg'),
+            // ),
+          ],
+        ),
+      ),
+    );
+
+    // show the dialog
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
+      },
+    );
+  }
+}
